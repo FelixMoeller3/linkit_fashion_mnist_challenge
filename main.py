def93ae25c2553c9ff0e8a32449a0f242cb93915
FelixMoeller3 <felix.moeller4@gmail.com>
2021-05-26 11:46:26 +0200

    This is the architecture I got 93,58% accuracy on.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from tensorflow.keras.datasets import fashion_mnist
 from tensorflow import keras
-from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten
+from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten, BatchNormalization
+from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 #import matplotlib.pyplot as plt
 #import tensorflow as tf
 
+NUM_EPOCHS = 25
+INIT_LR = 0.01
+BATCH_SIZE = 32
 
-#print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
 x_train = x_train.reshape(-1,28,28, 1)
@@ -17,15 +20,36 @@ x_test = x_test/255
 
 def build_model():
     model = keras.models.Sequential()
-    model.add(Conv2D(160, kernel_size=3, input_shape=x_train.shape[1:], activation='relu'))
+
+    model.add(Conv2D(32, kernel_size=3, input_shape=x_train.shape[1:], activation='relu', padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(32, kernel_size=3, activation='relu', padding='same'))
+    model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
-    model.add(Conv2D(96, kernel_size=3, activation='relu'))
-    model.add(Conv2D(64, kernel_size=3, activation='relu'))
+    model.add(Conv2D(64, kernel_size=3, input_shape=x_train.shape[1:], activation='relu', padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, kernel_size=3, activation='relu', padding='same'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    # model.add(Conv2D(128, kernel_size=3, input_shape=x_train.shape[1:], activation='relu', padding='same'))
+    # model.add(BatchNormalization())
+    # model.add(Conv2D(128, kernel_size=3, activation='relu', padding='same'))
+    # model.add(BatchNormalization())
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Dropout(0.25))
+
     model.add(Flatten())
-    model.add(Dense(10,activation='softmax'))
+    model.add(Dense(512, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
 
-    model.compile(optimizer='adam',
+    model.add(Dense(10, activation='softmax'))
+
+    model.compile(optimizer=SGD(lr=INIT_LR, momentum=0.9, decay=INIT_LR/NUM_EPOCHS),
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
@@ -34,5 +58,5 @@ def build_model():
 
 model = build_model()
 #model.summary()
-model.fit(x_train, y_train, batch_size=64, epochs=5, validation_data=(x_test, y_test), verbose=1)
+model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=NUM_EPOCHS, validation_data=(x_test, y_test), verbose=1)
 
