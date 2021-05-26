4ab847434139f6f648074b270f9a5c440ae01a6c
FelixMoeller3 <felix.moeller4@gmail.com>
2021-05-26 23:16:24 +0200

    I obtained >94% test accuracy using exactly this setup

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from tensorflow.keras.datasets import fashion_mnist
 from tensorflow import keras
 from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten, BatchNormalization
 from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 #import matplotlib.pyplot as plt
 #import tensorflow as tf
 
-NUM_EPOCHS = 25
-INIT_LR = 0.01
+NUM_EPOCHS = 35
+INIT_LR = 1e-2 * 0.5
 BATCH_SIZE = 32
+
+#gen = ImageDataGenerator(vertical_flip=True, horizontal_flip=True)
 
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
@@ -21,32 +24,29 @@ x_test = x_test/255
 def build_model():
     model = keras.models.Sequential()
 
-    model.add(Conv2D(32, kernel_size=3, input_shape=x_train.shape[1:], activation='relu', padding='same'))
+    model.add(Conv2D(32, kernel_size=3, activation='relu', input_shape=x_train.shape[1:], padding='same'))
     model.add(BatchNormalization())
     model.add(Conv2D(32, kernel_size=3, activation='relu', padding='same'))
     model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
-    model.add(Conv2D(64, kernel_size=3, input_shape=x_train.shape[1:], activation='relu', padding='same'))
+    model.add(Conv2D(64, kernel_size=3, activation='relu', padding='same'))
     model.add(BatchNormalization())
     model.add(Conv2D(64, kernel_size=3, activation='relu', padding='same'))
     model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
-    # model.add(Conv2D(128, kernel_size=3, input_shape=x_train.shape[1:], activation='relu', padding='same'))
-    # model.add(BatchNormalization())
-    # model.add(Conv2D(128, kernel_size=3, activation='relu', padding='same'))
-    # model.add(BatchNormalization())
-    # model.add(MaxPooling2D(pool_size=(2, 2)))
-    # model.add(Dropout(0.25))
 
     model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
+    model.add(Dense(256, activation='relu'))
     model.add(BatchNormalization())
-    model.add(Dropout(0.5))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.3))
 
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
     model.add(Dense(10, activation='softmax'))
 
     model.compile(optimizer=SGD(lr=INIT_LR, momentum=0.9, decay=INIT_LR/NUM_EPOCHS),
