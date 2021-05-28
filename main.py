12578ea46df3ace24c655aa6a78afb0714adde18
FelixMoeller3 <felix.moeller4@gmail.com>
2021-05-28 10:39:36 +0200

    I achieved 94.71% val_accuracy at best with this exact setup.

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ from tensorflow.keras.datasets import fashion_mnist
 from tensorflow import keras
 from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten, BatchNormalization
 from tensorflow.keras.optimizers import SGD
-from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.regularizers import l2
+#from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 #import matplotlib.pyplot as plt
 #import tensorflow as tf
 
-NUM_EPOCHS = 35
-INIT_LR = 1e-2 * 0.5
+NUM_EPOCHS = 200
+INIT_LR = 1e-3
 BATCH_SIZE = 32
+L2_PENALTY = 0.002
 
-#gen = ImageDataGenerator(vertical_flip=True, horizontal_flip=True)
+gen = ImageDataGenerator(horizontal_flip=True, zoom_range=0.1)
 
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
@@ -24,28 +26,32 @@ x_test = x_test/255
 def build_model():
     model = keras.models.Sequential()
 
-    model.add(Conv2D(32, kernel_size=3, activation='relu', input_shape=x_train.shape[1:], padding='same'))
+    model.add(Conv2D(32, kernel_size=3, activation='relu', input_shape=x_train.shape[1:], padding='same', kernel_regularizer=l2(L2_PENALTY)))
     model.add(BatchNormalization())
-    model.add(Conv2D(32, kernel_size=3, activation='relu', padding='same'))
+    model.add(Conv2D(32, kernel_size=3, activation='relu', padding='same', kernel_regularizer=l2(L2_PENALTY)))
+    model.add(BatchNormalization())
+    model.add(Conv2D(32, kernel_size=3, activation='relu', padding='same', kernel_regularizer=l2(L2_PENALTY)))
     model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
-    model.add(Conv2D(64, kernel_size=3, activation='relu', padding='same'))
+    model.add(Conv2D(64, kernel_size=3, activation='relu', padding='same', kernel_regularizer=l2(L2_PENALTY)))
     model.add(BatchNormalization())
-    model.add(Conv2D(64, kernel_size=3, activation='relu', padding='same'))
+    model.add(Conv2D(64, kernel_size=3, activation='relu', padding='same', kernel_regularizer=l2(L2_PENALTY)))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, kernel_size=3, activation='relu', padding='same', kernel_regularizer=l2(L2_PENALTY)))
     model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
 
     model.add(Flatten())
-    model.add(Dense(256, activation='relu'))
+    model.add(Dense(256, activation='relu', kernel_regularizer=l2(L2_PENALTY)))
     model.add(BatchNormalization())
-    model.add(Dense(256, activation='relu'))
-    model.add(Dropout(0.3))
+    model.add(Dense(256, activation='relu', kernel_regularizer=l2(L2_PENALTY)))
+    model.add(Dropout(0.25))
 
-    model.add(Dense(256, activation='relu'))
+    model.add(Dense(256, activation='relu', kernel_regularizer=l2(L2_PENALTY)))
     model.add(Dropout(0.5))
     model.add(Dense(10, activation='softmax'))
 
@@ -57,6 +63,6 @@ def build_model():
 
 
 model = build_model()
-#model.summary()
-model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=NUM_EPOCHS, validation_data=(x_test, y_test), verbose=1)
+model.summary()
+model.fit(gen.flow(x_train, y_train), batch_size=BATCH_SIZE, epochs=NUM_EPOCHS, validation_data=(x_test, y_test), verbose=1)
 
