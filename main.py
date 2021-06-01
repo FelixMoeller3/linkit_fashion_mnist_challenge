6ff02879033cd173aa346ce3d05bfe03e1c0b2f1
FelixMoeller3 <felix.moeller4@gmail.com>
2021-06-01 16:05:24 +0200

    Achieved 94.88% test accuracy using exactly this setup (in epoch 188/200)

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from tensorflow.keras.datasets import fashion_mnist
 from tensorflow import keras
-from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten, BatchNormalization
+from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten, BatchNormalization, GlobalAveragePooling2D
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.regularizers import l2
-from linkit_fashion_mnist_challenge.train_data import load_dataset
+from train_data import load_dataset
 #from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from random_eraser import get_random_eraser
 #import matplotlib.pyplot as plt
 #import tensorflow as tf
 
 NUM_EPOCHS = 200
 INIT_LR = 1e-3
 BATCH_SIZE = 32
-L2_PENALTY = 0.002
+L2_PENALTY = 0.003
 
 gen = ImageDataGenerator(horizontal_flip=True, zoom_range=0.1)
 
-(x_train, y_train), (x_test, y_test) = load_dataset()
+(x_train, y_train), (x_test, y_test) = load_dataset(3)
 
 def build_model():
     model = keras.models.Sequential()
